839c0fcbf28f2fd7a55e39162b96a678c8f7d37e
Laura Walters <laura.walters1098@yahoo.com>
2022-01-07 17:10:55 +0000

    completing if statements and error messages

--- main.js
+++ main.js
@@ -93,7 +93,8 @@ generateBtn.addEventListener("click", function () {
     "How many characters would you like to include in your password?"
   );
   if (chosenLength < 8 || chosenLength > 128) {
-    console.log("ERROR, Password must contain between 8 and 128 characters");
+    alert("ERROR, Password must contain between 8 and 128 characters");
+    return;
   } else {
     var incLower = confirm(
       "Press Ok to include lowercase characters in your password"
@@ -150,5 +151,153 @@ generateBtn.addEventListener("click", function () {
     }
     newPassword.textContent = password;
     console.log("lu");
+  } else if (incLower && !incUpper && !incNumber && !incSpecial) {
+    charArray = lowerCase.concat(lowerCase);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("l");
+  } else if (!incLower && !incUpper && !incNumber && !incSpecial) {
+    alert(
+      "ERROR! Please choose at least one character type to generate a password."
+    );
+  } else if (!incLower && incUpper && incNumber && incSpecial) {
+    charArray = upperCase.concat(numberChar, specialChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("uns");
+  } else if (!incLower && !incUpper && incNumber && incSpecial) {
+    charArray = numberChar.concat(specialChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("ns");
+  } else if (!incLower && !incUpper && !incNumber && incSpecial) {
+    charArray = specialChar.concat(specialChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("s");
+  } else if (incLower && !incUpper && !incNumber && incSpecial) {
+    charArray = lowerCase.concat(specialChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("is");
+  } else if (!incLower && incUpper && incNumber && !incSpecial) {
+    charArray = upperCase.concat(numberChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("un");
+  } else if (incLower && !incUpper && incNumber && incSpecial) {
+    charArray = lowerCase.concat(numberChar, specialChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("lns");
+  } else if (!incLower && incUpper && !incNumber && !incSpecial) {
+    charArray = upperCase.concat(upperCase);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("u");
+  } else if (incLower && incUpper && !incNumber && incSpecial) {
+    charArray = lowerCase.concat(upperCase, specialChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("lus");
+  } else if (!incLower && !incUpper && incNumber && !incSpecial) {
+    charArray = numberChar.concat(numberChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("n");
+  } else if (incLower && !incUpper && incNumber && !incSpecial) {
+    charArray = lowerCase.concat(numberChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("ln");
+  } else if (!incLower && incUpper && !incNumber && incSpecial) {
+    charArray = upperCase.concat(specialChar);
+    charString = charArray.join("");
+
+    for (var i = 0; i <= chosenLength - 1; i++) {
+      randomNumber = charString.charAt(
+        Math.floor(Math.random() * charArray.length)
+      );
+      password += randomNumber;
+    }
+    newPassword.textContent = password;
+    console.log("us");
   }
 });
